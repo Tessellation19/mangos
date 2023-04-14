07012e962f2ef0c51752b05eb82b888d66d8200c
Tessellation19 <127444710+Tessellation19@users.noreply.github.com>
2023-04-14 21:45:28 +0000

    TGIF

--- unit11/EulerFriday.java
+++ unit11/EulerFriday.java
@@ -12,7 +12,19 @@ public class EulerFriday {
         System.out.println(sum);
 
         // 2
-
+        sum = 0;
+        int a =1;
+        int b = 2;
+        while(b<4000000){
+            if(b%2==0){
+                sum+=b;
+            }
+            int c = a+b;
+            a=b;
+            b=c;
+            
+        }
+        System.out.println(sum);
     }
 
     static int[] prob8scaffold() {
